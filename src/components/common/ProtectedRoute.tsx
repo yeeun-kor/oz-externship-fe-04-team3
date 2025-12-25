351c1be00b1d22ebe28bf1b325fe968a37286fca
yeeun-kor <jovm268@naver.com>
2025-12-25 15:57:08 +0000

    feat: ProtectedRoute에서 GUEST 상태 시 리다이렉트 로직 개선 - 로그인 담당 1팀 도메인으로 리다이렉트 시킴

--- src/components/common/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
-import { Navigate, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { useAuthStore } from '@/store/userStore'
+import { useEffect } from 'react'
 
 interface ProtectedRouteProps {
   children: React.ReactNode
@@ -9,9 +10,17 @@ function ProtectedRoute({ children }: ProtectedRouteProps) {
   const loginState = useAuthStore((state) => state.loginState)
   const location = useLocation()
 
-  if (loginState === 'GUEST') {
-    return <Navigate to="/login" replace state={{ from: location.pathname }} />
-  }
+  useEffect(() => {
+    if (loginState === 'GUEST') {
+      const currentUrl = window.location.href
+      const redirectUrl = `https://account.ozcoding.site/login?from=${encodeURIComponent(
+        currentUrl
+      )}`
+      window.location.href = redirectUrl
+    }
+  }, [loginState, location])
+
+  if (loginState === 'GUEST') return null
 
   return children
 }
